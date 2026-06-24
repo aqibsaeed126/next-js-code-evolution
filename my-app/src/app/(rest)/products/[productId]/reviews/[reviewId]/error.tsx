a6d8379e96b837bb5f5d1f621652824d751f2e06
Aqib Saeed <saeedaqib295@gmail.com>
2026-06-24 16:59:13 +0500

    [day-6] - NextJs Code Evolution error special file

--- my-app/src/app/(rest)/products/[productId]/reviews/[reviewId]/error.tsx
+++ my-app/src/app/(rest)/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,26 @@
 'use client';
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div>{error.message}</div>;
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const handleTryAgain = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <>
+      <div>{error.message}</div>
+      <button onClick={() => handleTryAgain()}>Try Again</button>
+    </>
+  );
 }
--- my-app/src/app/(rest)/products/[productId]/reviews/[reviewId]/page.tsx
+++ my-app/src/app/(rest)/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,16 +1,28 @@
 import { notFound, redirect } from 'next/navigation';
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
-  const { productId, reviewId } = await params;
-  if (parseInt(reviewId) > 1000) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    // occasionally generate error
     //notFound();
     //redirect('/products');
     throw new Error('Review id greater than 1000 Error');
   }
+  const { productId, reviewId } = await params;
+
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+    //redirect('/products');
+  }
+
   return (
     <div>
       Details about Product {productId} Review {reviewId}
